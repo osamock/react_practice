0139fe489c4f80bc58c302f04c86e5bcb4fa9175
Kouta Osabe <kota0919wasabi@gmail.com>
2017-02-15 13:25:22 +0900

    modification before fetching data from server

--- public/tutorial1.js
+++ public/tutorial1.js
@@ -1,13 +1,64 @@
 // tutorial1.js
 // TODO You have to modify the code style from ES5 to ES6 style after the completion of this tutorial.
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+
 var CommentBox = React.createClass({
   render: function() {
   	return(
       <div className="commentBox">
-      	Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList data={this.props.data}/>
+        <CommentForm />
       </div>
     );
   }
 });
 
-ReactDOM.render(<CommentBox />,document.getElementById('content'));
+var CommentList = React.createClass({
+	render: function() {
+        var commentNodes = this.props.data.map(function(comment){
+          return(
+            <Comment author={comment.author}>
+              {comment.text}
+            </Comment>
+          	);
+        })
+
+		return(
+		  <div className="commentList">
+            {commentNodes}
+		  </div>
+	    );
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function() {
+		return(
+		  <div className="commentForm">
+            Hello, world! I am a CommentForm.
+		  </div>
+		);
+	}
+});
+
+var Comment = React.createClass({
+  render: function() {  	
+  	var rawMarkup = marked(this.props.children.toString(), {sanitize: false});
+    return(
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+    );
+  }
+}); 
+
+
+ReactDOM.render(<CommentBox data={data}/>,document.getElementById('content'));
